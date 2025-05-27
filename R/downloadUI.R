fc48134bd9827b6863e421f4f34d486b3f0d8266
Matthew Ponzini <mdponzini@ucdavis.edu>
2025-05-27 10:31:59 -0700

    first pass at adding "rds" as an export option. restrict download UI to those relevant for selected download (rds vs image file). quick and dirty approach, should be updated in future to proper module structure instead of copy/paste.
    update web browser tab name from 'CTSC Figures' to 'GoFiguR'

--- R/downloadUI.R
+++ R/downloadUI.R
@@ -4,37 +4,14 @@ downloadUI <- function(id){
   tagList(
     column(
       width = 8,
-      textInput(
-        inputId = NS(id, "plot_name"),
-        label = "Figure File Name:",
-        value = "figure-name"
-      ),
       selectInput(
         inputId = NS(id, "plot_device"),
         label = "Figure File Type:",
-        choices = c("jpeg", "png", "tiff", "pdf"),
-        selected = "jpeg",
+        choices = c("jpeg", "png", "tiff", "pdf", "rds"),
+        selected = NULL,
         multiple = FALSE
       ),
-      numericInput(
-        inputId = NS(id, "plot_height"),
-        label = "Figure Height:",
-        value = 6,
-        min = 1
-      ),
-      numericInput(
-        inputId = NS(id, "plot_width"),
-        label = "Figure Width:",
-        value = 6,
-        min = 1
-      ),
-      numericInput(
-        inputId = NS(id, "plot_dpi"),
-        label = "Figure Dots per Inch (DPI):",
-        value = 300,
-        min = 1,
-        max = 600
-      )
+      uiOutput(NS(id, "downloadOpts"))
     ),
     column(
       width = 4,
@@ -45,3 +22,50 @@ downloadUI <- function(id){
     )
   )
 }
+
+
+# downloadServer <- function(id) {
+#   shiny::moduleServer(id, function(input, output, session) {
+#     
+#     opts <- reactive({
+#       if(input$plot_device == "rds") {
+#         list(
+#           textInput(
+#             inputId = NS(id, "plot_name"),
+#             label = "Figure File Name:",
+#             value = "figure-name"
+#           )
+#         )
+#       } else {
+#         list(
+#           textInput(
+#             inputId = NS(id, "plot_name"),
+#             label = "Figure File Name:",
+#             value = "figure-name"
+#           ),
+#           numericInput(
+#             inputId = NS(id, "plot_height"),
+#             label = "Figure Height:",
+#             value = 6,
+#             min = 1
+#           ),
+#           numericInput(
+#             inputId = NS(id, "plot_width"),
+#             label = "Figure Width:",
+#             value = 6,
+#             min = 1
+#           ),
+#           numericInput(
+#             inputId = NS(id, "plot_dpi"),
+#             label = "Figure Dots per Inch (DPI):",
+#             value = 300,
+#             min = 1,
+#             max = 600
+#           )
+#         )
+#       }
+#     })
+#     
+#     output$downloadOpts <- renderUI(opts())
+#   })
+# }
